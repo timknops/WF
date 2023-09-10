27edc5914032a7f4eed1a41b71d47e254046200d
Julian Kruithof <julian.kruithof@hva.nl>
2023-09-10 19:59:41 +0200

    changed layout

--- auc-vue/src/components/HeaderComponent.vue
+++ auc-vue/src/components/HeaderComponent.vue
@@ -13,18 +13,18 @@ export default {
 <template>
   <header class="w-100 container-fluid bg-secondary text-white">
     <div class="d-flex">
-      <div class="left-img p-2 border-light">
+      <div class="left-img p-1 border-light">
         <img src="../assets/gavel.jpg" class="rounded" alt="picture of a gavel">
       </div>
-      <div class="center p-2 col">
-        <div class="title pb-2 border-light border-bottom">
+      <div class="center p-1 col">
+        <div class="title pb-1 border-light border-bottom">
           <h1 class="text-center">{{ title }}</h1>
         </div>
         <div class="sub-title">
           <h2 class="text-end">{{ subtitle}}</h2>
         </div>
       </div>
-      <div class="right-img p-2">
+      <div class="right-img p-1">
         <img src="../assets/auction-paddle.jpg" class="rounded" alt="Auction bidding paddle">
       </div>
     </div>
@@ -37,7 +37,7 @@ export default {
 img {
   object-fit: cover;
   aspect-ratio: 1;
-  height: 125px;
+  height: 100px;
 }
 
 </style>
