d17057144795042bd4a02438bccf1cbbe7f7303f
Julian Kruithof <julian.kruithof@hva.nl>
2023-10-11 18:18:13 +0200

    Implemented crud operations in repositories

--- auc-server/src/main/java/app/repositories/OffersRepository.java
+++ auc-server/src/main/java/app/repositories/OffersRepository.java
@@ -5,5 +5,30 @@ import java.util.List;
 
 public interface OffersRepository<E> {
 
+    /**
+     * find all available items
+     * @return a list of items
+     */
     List<E> findAll();
+
+    /**
+     * Find a specific item in the list
+     * @param id id of the item to be searched
+     * @return the item which was found, or null if the item doesn't exist.
+     */
+    E findById(long id);
+
+    /**
+     * Update the properties of the item, or insert a new item if the item doesn't exist
+     * @param item the item to be updated or inserted
+     * @return the new item
+     */
+    E save(E item);
+
+    /**
+     * Delete an item out of the list
+     * @param id the id of the item to be deleted
+     * @return the item which was deleted, or null if the item didn't exists
+     */
+    E deleteById(long id);
 }
--- auc-server/src/main/java/app/repositories/OffersRepositoryMock.java
+++ auc-server/src/main/java/app/repositories/OffersRepositoryMock.java
@@ -10,19 +10,47 @@ import java.util.List;
 public class OffersRepositoryMock implements OffersRepository<Offer> {
 
     private List<Offer> offers;
+    private final long STARTING_ID = 3000;
+    private long currentId = STARTING_ID;
 
     public OffersRepositoryMock() {
-        final int STARTING_ID = 3000, AMOUNT_OF_OFFERS = 7;
+        final int AMOUNT_OF_OFFERS = 7;
 
         this.offers = new ArrayList<>(AMOUNT_OF_OFFERS);
 
         for (int i = 0; i < AMOUNT_OF_OFFERS; i++) {
-            this.offers.add(Offer.createSampleOffer(STARTING_ID + i));
+            currentId++;
+            this.offers.add(Offer.createSampleOffer(currentId));
         }
     }
 
     @Override
     public List<Offer> findAll() {
         return offers;
+    }
+
+    @Override
+    public Offer findById(long id) {
+        return offers.stream().filter(offer -> offer.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public Offer save(Offer item) {
+        int index = offers.indexOf(item);
+        if (index != -1) {
+            offers.set(index, item);
+        } else {
+            offers.add(item);
+        }
+        return item;
+    }
+
+    @Override
+    public Offer deleteById(long id) {
+        Offer toBeDeleted = this.findById(id);
+        if (toBeDeleted != null) {
+            offers.remove(toBeDeleted);
+        }
+        return toBeDeleted;
     }
 }
