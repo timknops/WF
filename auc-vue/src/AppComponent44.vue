74de2cd3922615ac436a9191e6cd5955e213b1f7
Julian Kruithof <julian.kruithof@hva.nl>
2023-12-07 17:43:33 +0100

    completed header and app component 4.4

---
+++ auc-vue/src/AppComponent44.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderComponent />
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderSbComponent.vue";
+import NavBar from "@/components/NavBar.vue";
+import { OffersAdaptor } from "@/service/offersAdaptor";
+import Config from "@/config.js";
+import {SessionSbService} from "@/service/sessionSbService";
+import {shallowReactive} from "vue";
+
+export default {
+  name: "AppComponent44",
+  components: {
+    NavBar,
+    HeaderComponent,
+  },
+  provide() {
+    this.theSessionSevice = shallowReactive(new SessionSbService(`${Config.BACKEND_URL}/authentication`, Config.JWT_STORAGE_ITEM));
+    return {
+      offersService: new OffersAdaptor(`${Config.BACKEND_URL}/offers`),
+      sessionService: this.theSessionSevice,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ auc-vue/src/components/HeaderSbComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="w-100 container-fluid bg-body-tertiary border-bottom px-5">
+    <div class="d-flex">
+      <div class="left-img border-light pt-2">
+        <img
+            src="../assets/gavel.jpg"
+            class="rounded"
+            alt="picture of a gavel"
+        />
+      </div>
+      <div class="center col">
+        <div class="title pb-1 border-bottom mt-2">
+          <h1 class="text-center fw-bold text-primary">{{ title }}</h1>
+        </div>
+        <div class="sub-title align-items-center row px-2 py-2">
+          <h6 class="col m-0 my-2">Today is {{ getCurrentDate }}</h6>
+          <h6 class="col text-end m-0 my-2">{{ username }}</h6>
+        </div>
+      </div>
+      <div class="right-img pt-2">
+        <img
+            src="../assets/auction-paddle.jpg"
+            class="rounded"
+            alt="Auction bidding paddle"
+        />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderSbComponent",
+  inject: ["sessionService"],
+  data() {
+    return {
+      title: "Auctioneer",
+      subtitle: "Powered to you by hva.nl",
+    };
+  },
+  computed: {
+    getCurrentDate() {
+      const date = new Date();
+
+      return date.toLocaleDateString("en-IN", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    },
+    username() {
+      const user = this.sessionService.currentAccount
+      return user != null ? `Welcome ${user.name}!` : "Welcome visitor!"
+    }
+  },
+};
+</script>
+
+<style scoped>
+img {
+  object-fit: cover;
+  aspect-ratio: 1;
+  height: 100px;
+}
+</style>
